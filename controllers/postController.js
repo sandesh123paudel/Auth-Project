4ad2a83f9cfe179b53729e3f9799907fcea96f44
Sandesh Paudel <sandeshpaudel0618@gmail.com>
2025-07-03 12:05:50 +0545

    Add singlePost and updatePost methods in postController for retrieving and updating posts. Update postRouter to include routes for these functionalities, ensuring user identification middleware is applied for secure access.

--- controllers/postController.js
+++ controllers/postController.js
@@ -56,27 +56,61 @@ exports.createPost = async (req, res) => {
   }
 };
 
-
-exports.getSinglePost = async (req, res) => {
-  const { page } = req.query;
-  const postsPerPage = 10;
+exports.singlePost = async (req, res) => {
+  const { _id } = req.query;
 
   try {
-    let pageNum = 0;
-    if (page <= 1) {
-      pageNum = 0;
-    } else {
-      pageNum = page - 1;
+    const existingPost = await Post.findOne({ _id }).populate({
+      path: "userId",
+      select: "email",
+    });
+    if (!existingPost) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Post Unavailable" });
     }
-    const result = await Post.find()
-      .sort({ createdAt: -1 })
-      .skip(pageNum * postsPerPage)
-      .limit(postsPerPage)
-      .populate({
-        path: "userId",
-        select: "email",
-      });
-    res.status(200).json({ success: true, message: "posts", data: result });
+    res
+      .status(200)
+      .json({ success: true, message: "Single Post", data: existingPost });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.updatePost = async (req, res) => {
+  const { _id } = req.query;
+  const { title, description } = req.body;
+  const { userId } = req.user;
+
+  try {
+    const { error, value } = createPostSchema.validate({
+      title,
+      description,
+      userId,
+    });
+    if (error) {
+      return res
+        .status(401)
+        .json({ success: false, message: error.details[0].message });
+    }
+    const existingPost = await Post.findOne({ _id });
+    if (!existingPost) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Post Unavailable" });
+    }
+
+    if (existingPost.userId.toString() !== userId) {
+      return res.status(401).json({ success: false, message: "Unauthorized" });
+    }
+    existingPost.title = title;
+    existingPost.description = description;
+
+    const result = await existingPost.save();
+
+    res
+      .status(200)
+      .json({ success: true, message: "Post Updated ", data: result });
   } catch (error) {
     console.log(error);
   }
--- routers/postRouter.js
+++ routers/postRouter.js
@@ -4,9 +4,9 @@ const postContoller = require("../controllers/postController");
 const { identifier } = require("../middlewares/identification");
 
 postRouter.get("/all-posts", postContoller.getPosts);
-// postRouter.get("/single-post", postContoller.signUp);
+postRouter.get("/single-post", postContoller.singlePost);
 postRouter.post("/create-post", identifier, postContoller.createPost);
-// postRouter.put("/update-post", postContoller.signUp);
+postRouter.put("/update-post", identifier, postContoller.updatePost);
 // postRouter.delete("/delete-post", postContoller.signUp);
 
 module.exports = postRouter;
